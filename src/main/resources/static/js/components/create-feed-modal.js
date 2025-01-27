cbb9d2a74e96b1849dbd55e09e91d0982cddc367
hgb926 <hgb926@naver.com>
2025-01-27 13:57:17 +0900

    #02 open create feed modal

--- src/main/resources/static/js/components/create-feed-modal.js
+++ src/main/resources/static/js/components/create-feed-modal.js
@@ -1,7 +1,20 @@
+// 피드 생성 모달 전역관리
+let $modal = null;
 
 
-function initCreateFeedModal () {
-    console.log('모달관련 함수 호출');
+function initCreateFeedModal() {
+    $modal = document.getElementById("createPostModal");
+
+    // 모달 열기
+    const openModal = (e) => {
+        e.preventDefault()
+        $modal.style.display = 'flex'
+    }
+
+    // 피드 생성 모달 열기 이벤트
+    document.querySelector('.fa-square-plus')
+        .closest('.menu-item')
+        .addEventListener('click', openModal)
 }
 
 export default initCreateFeedModal;
