90fbe8d723c6655bffb7cffe4b3dacf933483735
hgb926 <hgb926@naver.com>
2025-01-28 14:29:13 +0900

    #16 [BE/config] add exception config

--- src/main/java/com/example/instagramclone/config/WebResourceConfig.java
+++ src/main/java/com/example/instagramclone/config/WebResourceConfig.java
@@ -1,7 +1,6 @@
 package com.example.instagramclone.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
